7d0cc70a2e8061af8927d167e387d97e87a2d2fc
J <j@JMacMini.local>
2026-04-17 08:43:33 -0500

    feat: wire up all routes and hide nav on landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,23 @@
+import { Routes, Route, useLocation } from 'react-router-dom'
+import Nav from './components/Nav/Nav'
+import Landing from './pages/Landing/Landing'
+import BrowseRecipes from './pages/BrowseRecipes/BrowseRecipes'
+import MyFridge from './pages/MyFridge/MyFridge'
+import RecipeDetail from './pages/RecipeDetail/RecipeDetail'
+
 export default function App() {
-  return <div id="app" />
+  const location = useLocation()
+  const isLanding = location.pathname === '/'
+
+  return (
+    <>
+      {!isLanding && <Nav />}
+      <Routes>
+        <Route path="/"            element={<Landing />} />
+        <Route path="/recipes"     element={<BrowseRecipes />} />
+        <Route path="/recipes/:id" element={<RecipeDetail />} />
+        <Route path="/fridge"      element={<MyFridge />} />
+      </Routes>
+    </>
+  )
 }
