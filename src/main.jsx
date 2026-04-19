ba8ad974b901934a1daa3964b0d02df460f0929a
J <j@JMacMini.local>
2026-04-18 23:52:56 -0500

    feat: add ClerkProvider to React root

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { ClerkProvider } from '@clerk/clerk-react'
 import './styles/tokens.css'
 import './App.css'
 import App from './App.jsx'
 
+const publishableKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ClerkProvider publishableKey={publishableKey}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ClerkProvider>
   </StrictMode>
 )
